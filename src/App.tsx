f3253c341b80f736f40af9b74c06a58ddd9b4ccc
3DBioCAD_Tech <chchch1213@naver.com>
2022-12-23 11:51:54 -0800

    Add Movement

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
-import { Group } from "@mantine/core";
-import { IconMicrophone } from "@tabler/icons";
-import { useState } from "react";
+import { ActionIcon, Box, useMantineTheme } from "@mantine/core";
+import { IconMicrophone, IconPlayerPlay, IconPlayerStop } from "@tabler/icons";
+import { useState, MouseEvent } from "react";
 
-const ICON_SIZE = 192;
+const ICON_SIZE = 192,
+  GAP = 16;
 const STATS = {
   NONE: 0,
   RECORD: 1,
@@ -12,31 +13,111 @@ const STATS = {
 };
 
 function App() {
+  const theme = useMantineTheme();
   const [stat, setStat] = useState(STATS.NONE);
 
+  const COLORS = {
+    BACK: [
+      theme.colors.gray[0],
+      theme.colors.red[1],
+      theme.colors.green[1],
+      theme.colors.blue[1],
+    ],
+    FRONT: [
+      theme.colors.gray[7],
+      theme.colors.red[8],
+      theme.colors.green[8],
+      theme.colors.blue[8],
+    ],
+  };
+
+  function onRecord(event: MouseEvent) {
+    event.stopPropagation();
+    setStat(STATS.RECORD);
+  }
+  function onPlay(event: MouseEvent) {
+    if (stat !== STATS.DONE) return;
+    event.stopPropagation();
+    setStat(STATS.PLAY);
+  }
+  function onStop(event: MouseEvent) {
+    if (stat !== STATS.PLAY) return;
+    event.stopPropagation();
+    setStat(STATS.DONE);
+  }
+
   return (
-    <Group
-      sx={(theme) => ({
-        backgroundColor:
-          stat === STATS.NONE
-            ? theme.colors.gray[0]
-            : stat === STATS.RECORD
-            ? theme.colors.red[1]
-            : stat === STATS.DONE
-            ? theme.colors.green[1]
-            : theme.colors.blue[1],
+    <Box
+      sx={{
+        backgroundColor: COLORS.BACK[stat],
         height: "100%",
         cursor: "pointer",
-        transition: "backgroundColor 0.2s",
-      })}
-      position="center"
-      align="center"
+        transition: "background-color 0.2s",
+      }}
       onClick={() => {
-        setStat(stat === STATS.PLAY ? STATS.NONE : stat + 1);
+        setStat(stat === STATS.PLAY ? STATS.RECORD : stat + 1);
       }}
     >
-      <IconMicrophone size={ICON_SIZE}></IconMicrophone>
-    </Group>
+      <ActionIcon
+        style={{
+          position: "absolute",
+          top: `calc(50% - ${ICON_SIZE / 2}px)`,
+          left: `calc(50% - ${ICON_SIZE / 2}px - ${
+            stat <= STATS.RECORD ? 0 : (ICON_SIZE + GAP) / 2
+          }px)`,
+          transition: "top 0.2s, left 0.2s",
+        }}
+        variant="transparent"
+        size={ICON_SIZE}
+        onClick={onRecord}
+      >
+        <IconMicrophone
+          style={{ transition: "stroke 0.2s" }}
+          color={COLORS.FRONT[stat]}
+          size={ICON_SIZE}
+        />
+      </ActionIcon>
+      <ActionIcon
+        style={{
+          position: "absolute",
+          top: `calc(50% - ${ICON_SIZE / 2}px - ${
+            stat === STATS.DONE ? 0 : ICON_SIZE
+          }px)`,
+          left: `calc(50% + ${GAP / 2}px)`,
+          opacity: stat === STATS.DONE ? 1 : 0,
+          transition: "top 0.2s, left 0.2s, opacity 0.2s",
+        }}
+        variant="transparent"
+        size={ICON_SIZE}
+        onClick={onPlay}
+      >
+        <IconPlayerPlay
+          style={{ transition: "stroke 0.2s" }}
+          color={COLORS.FRONT[stat]}
+          size={ICON_SIZE}
+        />
+      </ActionIcon>
+      <ActionIcon
+        style={{
+          position: "absolute",
+          top: `calc(50% - ${ICON_SIZE / 2}px + ${
+            stat === STATS.PLAY ? 0 : ICON_SIZE
+          }px)`,
+          left: `calc(50% + ${GAP / 2}px)`,
+          opacity: stat === STATS.PLAY ? 1 : 0,
+          transition: "top 0.2s, left 0.2s, opacity 0.2s",
+        }}
+        variant="transparent"
+        size={ICON_SIZE}
+        onClick={onStop}
+      >
+        <IconPlayerStop
+          style={{ transition: "stroke 0.2s" }}
+          color={COLORS.FRONT[stat]}
+          size={ICON_SIZE}
+        />
+      </ActionIcon>
+    </Box>
   );
 }
 

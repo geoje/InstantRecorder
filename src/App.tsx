c4a1438212de26bcc156bffa76d5e08a61c93c07
Gyeongho Yang <chchch1213@naver.com>
2022-12-22 22:01:38 -0800

    Set page color

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
 import "./App.css";
-import { Stack } from "@mantine/core";
-import { ActionIcon } from "@mantine/core/lib/ActionIcon";
+import { Group } from "@mantine/core";
 import { IconMicrophone } from "@tabler/icons";
+import { useState } from "react";
+
+const ICON_SIZE = 192;
+const STATS = {
+  NONE: 0,
+  RECORD: 1,
+  DONE: 2,
+  PLAY: 3,
+};
 
 function App() {
+  const [stat, setStat] = useState(STATS.NONE);
+
   return (
-    <Stack>
-      <ActionIcon>
-        <IconMicrophone />
-      </ActionIcon>
-    </Stack>
+    <Group
+      sx={(theme) => ({
+        backgroundColor:
+          stat === STATS.NONE
+            ? theme.colors.gray[0]
+            : stat === STATS.RECORD
+            ? theme.colors.red[1]
+            : stat === STATS.DONE
+            ? theme.colors.green[1]
+            : theme.colors.blue[1],
+        height: "100%",
+        cursor: "pointer",
+        transition: "backgroundColor 0.2s",
+      })}
+      position="center"
+      align="center"
+      onClick={() => {
+        setStat(stat === STATS.PLAY ? STATS.NONE : stat + 1);
+      }}
+    >
+      <IconMicrophone size={ICON_SIZE}></IconMicrophone>
+    </Group>
   );
 }
 

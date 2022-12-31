bce4cb33cac683cb4e3fe03896d8b9cfa7e06f80
Gyeongho Yang <chchch1213@naver.com>
2022-12-30 16:54:15 -0800

    Remove annotation

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 import { MantineProvider } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <MantineProvider withNormalizeCSS withGlobalStyles>
     <NotificationsProvider>
       <App />
@@ -16,7 +13,4 @@ root.render(
   </MantineProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
